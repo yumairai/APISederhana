d82a132a03402f6cc973cd950eb0c27c63e11b66
yumairai <yumairai3012@gmail.com>
2026-03-24 23:25:35 +0700

    .

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let todos = [];
+
+app.get("/todos", (req, res) => {
+  res.json({ status: "success", data: todos });
+});
+
+app.post("/todos", (req, res) => {
+  const todo = req.body;
+  todos.push(todo);
+  res.json({ status: "success", data: todo });
+});
+
+app.listen(3000, () => console.log("Server jalan"));
